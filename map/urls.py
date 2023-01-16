521072873f7cfc673db0b9294e2a01a04572ac38
lifeflavor0425 <rjsdnakfmzh61@gmail.com>
2023-01-16 19:18:01 +0900

    Django 동작 확인 & 거의 완성

--- map/urls.py
+++ map/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.shortcuts import redirect
+from django.views.generic.base import RedirectView
+
 urlpatterns = [
     path("", views.index, name="home"),
     path("station_line_search/", views.station_line_search, name="station_line_search"),
-    
+    path("404/", views.None_404, name="Noen_404"),
+    path("charts/", views.charts, name="charts"),
+    path("table/", views.table, name="table"),
 ]
--- map/views.py
+++ map/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from map.make_map import make_map, defalut_map, count_
+from map.modules.make_map import make_map, defalut_map, count_
+import pandas as pd
+import json
 
 # Create your views here.
 def index(request):
@@ -13,11 +15,31 @@ def station_line_search(request):
     print(request.POST.get("search_line"), request.POST.get("search_time"))
     input_line = request.POST.get("search_line")
     input_time = request.POST.get("search_time")
-    if input_line == '노선선택' or input_time == '시간선택':
+    if input_line == "노선선택" or input_time == "시간선택":
         map = defalut_map()
-        maps = map._repr_html_() 
-    else :
+        maps = map._repr_html_()
+    else:
         map = make_map(input_line)
         map = count_(input_line, input_time, map)
         maps = map._repr_html_()
     return render(request, "map/index.html", {"map": maps})
+
+
+def None_404(request):
+    return render(
+        request,
+        "map/404.html",
+    )
+
+
+def charts(request):
+    return render(
+        request,
+        "map/charts.html",
+    )
+
+
+def table(request):
+    result_df = pd.read_excel("./map/public/result_df.xlsx")
+    result_df = result_df.drop(columns=["Unnamed: 0.1", "Unnamed: 0"])
+    return render(request, "map/table.html", {"result_df": result_df})
